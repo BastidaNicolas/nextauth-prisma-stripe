ac175878422d4d7024bae222245cf7bed5108219
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-05 19:49:35 -0300

    create stripe customer and save customer in db.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import prisma from "../../../../../prisma/prisma";
+import Stripe from "stripe";
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -13,6 +14,27 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  events:{
+    createUser: async ({ user }) => {
+
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+
+      await stripe.customers.create({
+        email: user.email!,
+        name: user.name!,
+      })
+      .then( async (customer) => {
+        return prisma.user.update({
+          where: { id: user.id },
+          data: {
+            stripeCustomerId: customer.id,
+          },
+        });
+      })
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
