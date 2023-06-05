27cdd0c7533f4d85c8b794f8b62bc473a3fe9196
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-05 12:52:49 -0300

    secret added

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -14,6 +14,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET
 });
 
 export { handler as GET, handler as POST };
