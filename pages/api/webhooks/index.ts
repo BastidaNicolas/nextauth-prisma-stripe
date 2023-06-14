0628200c45c705d09acc206145c30734636f9f47
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-06-14 10:21:25 -0300

    copied nextjs documentation for stripe webhook

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -107,77 +107,73 @@
 // export default cors(handler as any);
 // // export default handler;
 
-import Stripe from 'stripe';
-import {NextApiRequest, NextApiResponse} from 'next';
+import { NextApiRequest, NextApiResponse } from 'next'
 
+import Cors from 'micro-cors'
+import Stripe from 'stripe'
+import { buffer } from 'micro'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: '2022-11-15',
+})
+
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!
+
+// Stripe requires the raw body to construct the event.
 export const config = {
   api: {
     bodyParser: false,
   },
-};
+}
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-): Promise<void> => {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2022-11-15',
-  });
+const cors = Cors({
+  allowMethods: ['POST', 'HEAD'],
+})
 
-  const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
-
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const sig = req.headers['stripe-signature'] as string;
+    const buf = await buffer(req)
+    const sig = req.headers['stripe-signature']!
 
-    let event: Stripe.Event;
+    let event: Stripe.Event
 
     try {
-      const body = await buffer(req);
-      event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret)
     } catch (err) {
-      // On error, log and return the error message
-      console.log(`❌ Error message: ${err}`);
-      res.status(400).send(`Webhook Error: ${err}`);
-      return;
+      const errorMessage = err instanceof Error ? err.message : 'Unknown error'
+      // On error, log and return the error message.
+      if (err! instanceof Error) console.log(err)
+      console.log(`❌ Error message: ${errorMessage}`)
+      res.status(400).send(`Webhook Error: ${errorMessage}`)
+      return
     }
 
-    // Successfully constructed event
-    console.log('✅ Success:', event.id);
+    // Successfully constructed event.
+    console.log('✅ Success:', event.id)
 
-    // Cast event data to Stripe object
+    // Cast event data to Stripe object.
     if (event.type === 'payment_intent.succeeded') {
-      const stripeObject: Stripe.PaymentIntent = event.data
-        .object as Stripe.PaymentIntent;
-      console.log(`💰 PaymentIntent status: ${stripeObject.status}`);
+      const paymentIntent = event.data.object as Stripe.PaymentIntent
+      console.log(`💰 PaymentIntent status: ${paymentIntent.status}`)
+    } else if (event.type === 'payment_intent.payment_failed') {
+      const paymentIntent = event.data.object as Stripe.PaymentIntent
+      console.log(
+        `❌ Payment failed: ${paymentIntent.last_payment_error?.message}`
+      )
     } else if (event.type === 'charge.succeeded') {
-      const charge = event.data.object as Stripe.Charge;
-      console.log(`💵 Charge id: ${charge.id}`);
+      const charge = event.data.object as Stripe.Charge
+      console.log(`💵 Charge id: ${charge.id}`)
     } else {
-      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`);
+      console.warn(`🤷‍♀️ Unhandled event type: ${event.type}`)
     }
 
-    // Return a response to acknowledge receipt of the event
-    res.json({received: true});
+    // Return a response to acknowledge receipt of the event.
+    res.json({ received: true })
   } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
   }
-};
+}
 
-const buffer = (req: NextApiRequest) => {
-  return new Promise<Buffer>((resolve, reject) => {
-    const chunks: Buffer[] = [];
-
-    req.on('data', (chunk: Buffer) => {
-      chunks.push(chunk);
-    });
-
-    req.on('end', () => {
-      resolve(Buffer.concat(chunks));
-    });
-
-    req.on('error', reject);
-  });
-};
-
-export default handler;
+export default cors(webhookHandler as any)
